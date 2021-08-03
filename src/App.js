402accb99d78174a7ffa58dbd9531846b74b6c4b
FREEDOM <freewhynane62@gmail.com>
2021-08-03 14:36:28 +0000

    add userFunction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import UsersClass from './Components/UsersClass';
+import UserFunction from './Components/UserFunction';
+
 
 function App() {
   return (
     <div className="App">
-      <UsersClass />
+      <UserFunction />
     </div>
   );
 }
